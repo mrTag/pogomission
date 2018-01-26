1711c00fb4760bfa64dabc75a45cdca385977a4d
Paul Lawitzki <p.lawitzki@gmail.com>
2018-01-26 21:01:24 +0100

    Camera: first implementation

--- Assets/Camera/CameraMan.cs
+++ Assets/Camera/CameraMan.cs
@@ -1,31 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 [RequireComponent(typeof(Camera))]
 public class CameraMan : MonoBehaviour {
 
     public static CameraMan sCamera = null;
 
-    public Transform ObjectToFollow;
+    public CameraTarget Target;
+    public float SmoothMinTime = .2f;
     [HideInInspector]
-
     public Camera Cam { get; private set; }
+    private SmoothDampedVector3 _position;
 
     void Start()
     {
         sCamera = this;
         Application.targetFrameRate = 120;
         Cam = GetComponent<Camera>();
+        _position = new SmoothDampedVector3(Target.PointOfInterest, SmoothMinTime);
     }
 
     void LateUpdate ()
     {
-        if (ObjectToFollow != null)
+        if (Target != null)
         {
+            _position.Target = Target.PointOfInterest;
+            _position.DoUpdate();
             transform.position = new Vector3(
-                ObjectToFollow.position.x,
-                ObjectToFollow.position.y,
+                _position.Current.x,
+                _position.Current.y,
                 transform.position.z);
         }
     }
---
+++ Assets/Camera/SmoothDampedUtil.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothDampedValue {
+	public float Target;
+	public float Current { get; private set; }
+	public float MinTime;
+	private float _velocity;
+
+	public SmoothDampedValue(float initValue, float minTime) {
+		Target = Current = initValue;
+		MinTime = minTime;
+	}
+
+	public void DoUpdate () {
+		Current = Mathf.SmoothDamp(Current, Target, ref _velocity, MinTime);
+	}
+}
+
+public class SmoothDampedVector3 {
+	public Vector3 Target;
+	public Vector3 Current { get; private set; }
+	public float MinTime;
+	private Vector3 _velocity;
+
+	public SmoothDampedVector3(Vector3 initVector, float minTime) {
+		Target = Current = initVector;
+		MinTime = minTime;
+	}
+
+	public void DoUpdate () {
+		Current = Vector3.SmoothDamp(Current, Target, ref _velocity, MinTime);
+	}
+}
