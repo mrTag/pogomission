ad04b8202cf3bf53f0c1c074cb52c6c77a9e319d
dominik <dominik@chasing-carrots.com>
2018-01-26 23:56:06 +0100

    steering ist jetzt absolut

--- Assets/pogophysics/PogoPusher.cs
+++ Assets/pogophysics/PogoPusher.cs
@@ -28,7 +28,7 @@ public class PogoPusher : MonoBehaviour {
 		if(raycastHit){
 			float distanceFactor = Mathf.Pow((1.0f - raycastHit.fraction), PushExpo);
 			float normalFactor = Mathf.Max(0, 90.0f - Vector2.Angle(-worldSpaceDir, raycastHit.normal)) / 90.0f;
-			normalFactor = Mathf.Pow(normalFactor, 2);
+			normalFactor = Mathf.Pow(normalFactor, 4);
 			Debug.Log("norm: "+normalFactor+" dist: "+distanceFactor);
 			_rigidbody.AddForceAtPosition(-worldSpaceDir * MaxPushForce * Time.fixedDeltaTime * distanceFactor * normalFactor, worldSpaceStart);
 		}
