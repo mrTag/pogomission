0751c3569965bf2f273f8cd83ba7fd71e99156bb
Patrick Wachowiak <patrick@chasing-carrots.com>
2018-01-27 12:00:59 +0100

    postman visual

---
+++ Assets/postman_visual.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class postman_visual : MonoBehaviour {
+
+	public GameObject jumper;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.SetParent(jumper.transform,false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
