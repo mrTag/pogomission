f531f473c79bba979b01f428a2abc56deb0ccfee
Paul Lawitzki <p.lawitzki@gmail.com>
2018-01-27 09:40:50 +0100

    camera setup

--- Assets/Camera/CameraTarget.cs
+++ Assets/Camera/CameraTarget.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class CameraTarget : MonoBehaviour
 {
-    public float VelocityAdvanceFactor = 1f;
+    public float VelocityAdvanceFactorX = 1f;
+    public float VelocityAdvanceFactorY = 0f;
     public float MaxAdvanceDistance = 5f;
     public Vector3 PointOfInterest { get; private set; }
 	private Rigidbody2D _rb;
@@ -14,7 +15,9 @@ public class CameraTarget : MonoBehaviour
 
     void Update () {
         if (_rb != null) {
-            Vector3 advanceVector = Vector3.ClampMagnitude(_rb.velocity * VelocityAdvanceFactor, MaxAdvanceDistance);
+            Vector3 advanceVector = Vector3.ClampMagnitude(
+                new Vector3(_rb.velocity.x * VelocityAdvanceFactorX, _rb.velocity.y * VelocityAdvanceFactorY, 0f),
+                MaxAdvanceDistance);
             PointOfInterest = transform.position + advanceVector;
         } else {
             PointOfInterest = transform.position;
