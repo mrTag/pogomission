966cf82c415c25e7da6b012d875e62a8d6cb4721
Patrick Wachowiak <patrick@chasing-carrots.com>
2018-01-28 10:44:39 +0100

    delivery

---
+++ Assets/models/delivery.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class delivery : MonoBehaviour {
+
+	public Transform MyPackage;
+
+	void Start() {
+		MyPackage.localPosition = new Vector3(0,10,0);
+	}
+
+	void OnEnable() {		
+		MyPackage.DOLocalMoveY(0,0.4f);
+
+	}
+	void OnDisable() {		
+		MyPackage.localPosition = new Vector3(0,10,0);
+	}
+}
